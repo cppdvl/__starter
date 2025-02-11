c427fe999e18e6239bf38ea727baed5ef8c8dfb6
Julian Guarin <devsavant.jguarin@intelepeer.com>
2025-02-11 08:51:21 -0500

    Ivy for telescope tlg and tff. Exafunction Codeium....

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -24,17 +24,30 @@ return {
     },
     config = function()
       print("Telescope config loaded")
-            require('telescope').setup {
-                pickers = {
-                    find_files = { theme = "ivy" },
-                    live_grep = { theme = "ivy" },
-                }
-            }
+      require('telescope').setup {
+        pickers = {
+          find_files = { theme = "ivy" },
+          live_grep = { theme = "ivy" },
+        }
+      }
       -- vim.keymap.set("n", "<space>tlg", require('telescope.builtin').live_grep(require('telescope.themes').get_ivy))
       local opts = require('telescope.themes').get_ivy({})
       require('telescope.builtin').live_grep(opts)
     end
-  }
+  },
+
+  {
+    "Exafunction/codeium.nvim",
+    lazy = false,
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "hrsh7th/nvim-cmp",
+    },
+    config = function()
+      require("codeium").setup({
+      })
+    end
+  },
   -- 	"nvim-treesitter/nvim-treesitter",
   -- 	opts = {
   -- 		ensure_installed = {
