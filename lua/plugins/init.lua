51d3bebeb8c597dd91e9a3efb43a0827a8cac2bc
cppdvl <cpp.dvl@gmail.com>
2025-02-21 18:49:36 -0500

    Copilot

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -35,19 +35,29 @@ return {
       require('telescope.builtin').live_grep(opts)
     end
   },
-
   {
-    "Exafunction/codeium.nvim",
-    lazy = false,
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "hrsh7th/nvim-cmp",
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    build = ":Copilot auth",
+    event = "BufReadPost",
+    opts = {
+      suggestion = {
+        enabled = not vim.g.ai_cmp,
+        auto_trigger = true,
+        hide_during_completion = vim.g.ai_cmp,
+        keymap = {
+          accept = "<M-\\>",
+          next = "<M-]>",
+          prev = "<M-[>",
+        },
+      },
+      panel = { enabled = false },
+      filetypes = {
+        markdown = true,
+        help = true,
+      },
     },
-    config = function()
-      require("codeium").setup({
-      })
-    end
-  },
+  }
   -- 	"nvim-treesitter/nvim-treesitter",
   -- 	opts = {
   -- 		ensure_installed = {
